0a8db687c6f7a382e4691e5db43b08be7376b994
Dean B <dean@deanveloper.com>
2016-11-05 16:30:37 -0400

    Added dummy SteVa

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -2,14 +2,18 @@ package org.devathon.contest2016;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class DevathonPlugin extends JavaPlugin {
     private static DevathonPlugin instance;
     private static World mainWorld;
+    private Model dummy;
 
     public static DevathonPlugin getInstance() {
         return instance;
@@ -38,6 +42,23 @@ public class DevathonPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         mainWorld.getEntitiesByClass(ArmorStand.class).forEach(Entity::remove);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+        if (sender instanceof Player) {
+            if (args.length == 0) {
+                Player p = (Player) sender;
+                if(dummy != null) {
+                    dummy.setLoc(p.getLocation());
+                } else {
+                    dummy = new Model(p.getLocation());
+                }
+            } else if (args[0].equalsIgnoreCase("clear")){
+                dummy.destroy();
+                dummy = null;
+            }
+        }
     }
 }
 
